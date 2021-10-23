fd9c2e60c92e591ad95ef5b44115431bbe00c306
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2021-10-23 18:21:10 +0300

    Update new old semester static file generation script

--- semesters/generate.sh
+++ semesters/generate.sh
@@ -1,4 +1,9 @@
-OLD_SEMESTER="w20_21"
+#! /usr/bin/env zsh
+
+set -ex
+set +o rmstarsilent
+
+OLD_SEMESTER="sp21"
 docker-compose exec site jekyll build --destination "semesters/${OLD_SEMESTER}" --baseurl "cs236781/semesters/${OLD_SEMESTER}"
-rm -rf "semesters/${OLD_SEMESTER}/semesters/*"
+rm -rf semesters/${OLD_SEMESTER}/semesters/*
 cp _site/semesters/index.html "semesters/${OLD_SEMESTER}/semesters"
